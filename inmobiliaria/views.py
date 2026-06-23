679ea1bb821c2056af23386f79ee549d0a20dca7
Christopher <cristpohereduardo2004@gmail.com>
2026-06-23 14:53:22 -0500

    Propiedad

--- inmobiliaria/views.py
+++ inmobiliaria/views.py
@@ -67,3 +67,82 @@ def inicio(request):
         context["total_contratos"] = contratos.count()
 
     return render(request, "inicio.html", context)
+
+# -------------------------
+# PROPIEDADES
+# -------------------------
+
+@login_required
+def listarPropiedades(request):
+    propiedades = Propiedad.objects.all()
+
+    return render(
+        request,
+        'listarPropiedades.html',
+        {'propiedades': propiedades}
+    )
+
+
+@login_required
+def nuevaPropiedad(request):
+    return render(request, 'nuevaPropiedad.html')
+
+
+@login_required
+def guardarPropiedad(request):
+
+    foto = request.FILES.get('foto')
+
+    Propiedad.objects.create(
+        agente=request.user,
+        foto=foto,
+        direccion=request.POST['direccion'],
+        tipo=request.POST['tipo'],
+        operacion=request.POST['operacion'],
+        precio=request.POST['precio'],
+        estacionamiento='estacionamiento' in request.POST
+    )
+
+    return redirect('listarPropiedades')
+
+
+@login_required
+def editarPropiedad(request, id):
+    propiedad = Propiedad.objects.get(id=id)
+
+    return render(
+        request,
+        'editarPropiedad.html',
+        {'propiedad': propiedad}
+    )
+
+
+@login_required
+def actualizarPropiedad(request):
+
+    propiedad = Propiedad.objects.get(
+        id=request.POST['id']
+    )
+
+    propiedad.direccion = request.POST['direccion']
+    propiedad.tipo = request.POST['tipo']
+    propiedad.operacion = request.POST['operacion']
+    propiedad.precio = request.POST['precio']
+    propiedad.estacionamiento = 'estacionamiento' in request.POST
+
+    nuevaFoto = request.FILES.get('foto')
+
+    if nuevaFoto:
+        propiedad.foto = nuevaFoto
+
+    propiedad.save()
+
+    return redirect('listarPropiedades')
+
+
+@login_required
+def eliminarPropiedad(request, id):
+    propiedad = Propiedad.objects.get(id=id)
+    propiedad.delete()
+
+    return redirect('listarPropiedades')
