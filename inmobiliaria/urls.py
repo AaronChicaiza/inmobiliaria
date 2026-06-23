bdb5085c4a405c23abdc18069839e2bd133a41d3
Aaron <chicaizaaaron@gmail.com>
2026-06-23 15:06:02 -0500

    urlscontratos

--- inmobiliaria/urls.py
+++ inmobiliaria/urls.py
@@ -14,4 +14,12 @@ urlpatterns = [
     path('editarPropiedad/<int:id>/', views.editarPropiedad, name='editarPropiedad'),
     path('actualizarPropiedad/', views.actualizarPropiedad, name='actualizarPropiedad'),
     path('eliminarPropiedad/<int:id>/', views.eliminarPropiedad, name='eliminarPropiedad'),
+
+    # Contratos
+    path('listarContratos/', views.listarContratos, name='listarContratos'),
+    path('nuevoContrato/', views.nuevoContrato, name='nuevoContrato'),
+    path('guardarContrato/', views.guardarContrato, name='guardarContrato'),
+    path('editarContrato/<int:id>/', views.editarContrato, name='editarContrato'),
+    path('actualizarContrato/', views.actualizarContrato, name='actualizarContrato'),
+    path('eliminarContrato/<int:id>/', views.eliminarContrato, name='eliminarContrato'),
 ]
