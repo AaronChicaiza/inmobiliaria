d7d25e9d85228eae8f454ff098bb67d4cec52fe0
Christopher <cristpohereduardo2004@gmail.com>
2026-06-23 14:44:42 -0500

    Urls

--- inmobiliaria/urls.py
+++ inmobiliaria/urls.py
@@ -2,7 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),  # 🔥 IMPORTANTE
+    # Login
+    path('', views.login_view, name='login'),
     path('inicio/', views.inicio, name='inicio'),
     path('logout/', views.logout_view, name='logout'),
+
+    # Propiedades
+    path('propiedades/', views.listarPropiedades, name='listarPropiedades'),
+    path('nuevaPropiedad/', views.nuevaPropiedad, name='nuevaPropiedad'),
+    path('guardarPropiedad/', views.guardarPropiedad, name='guardarPropiedad'),
+    path('editarPropiedad/<int:id>/', views.editarPropiedad, name='editarPropiedad'),
+    path('actualizarPropiedad/', views.actualizarPropiedad, name='actualizarPropiedad'),
+    path('eliminarPropiedad/<int:id>/', views.eliminarPropiedad, name='eliminarPropiedad'),
 ]
